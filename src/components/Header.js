34f9750a31dcbec768f9bbfd04fc7504336b30b1
PuppeJr <puppejunior@gmail.com>
2023-08-26 20:34:43 -0300

    novo layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Header = ({ cart, setShow }) => {
-  const { total, totalQuantity } = cart.reduce((acc, cur) => {
-    return {
-      total: acc.total + (cur.quantity > 0 ? cur.price * cur.quantity : 0),
-      totalQuantity: acc.totalQuantity + cur.quantity
-    };
-  }, { total: 0, totalQuantity: 0 });
-  
+  const total = cart.reduce((acc, cur) => {
+    if (cur.quantity > 0) {
+      return acc + cur.price * cur.quantity;
+    }
+    return acc;
+  }, 0);
+
   return (
-    <header className="bg-dark text-white">
+    <header className="bg-dark text-white fixed-top">
       <div className="container d-flex justify-content-between align-items-center py-3">
-        <div className="company-name h3 mb-0">Cuca Sagrada </div>
-        <p className="mb-0">Não temos Tele-Entrega!</p>
-        <p className="mb-0">Retirada no Local</p>
-        <p className="mb-0">Faça seu pedido com um dia de antecedência</p>
-        <p className="mb-0">Pagamento Dinheiro ou via PIX chave: 51989892020</p>
+        <div className="company-name h3 mb-0">Shopez</div>
         <div className="cart text-end">
-          <span className="cart-count me-3">Você já selecionou: {totalQuantity} {totalQuantity === 1 ? 'item' : 'itens'}.</span>
-          <span className="cart-total">Valor Total: R${total.toFixed(2)}</span>
-            <button className="btn btn-primary btn-sm" onClick={() => setShow(true)}>
-              Ver Carrinho
-            </button>
+          <span className="cart-count me-3">{cart.length} items</span>
+          <span className="cart-total">${total.toFixed(2)}</span>
+          <button className="btn btn-primary btn-sm" onClick={() => setShow(true)}>View cart</button>
         </div>
       </div>
     </header>
-  );
+  )
 };
 
 Header.propTypes = {
