ee8669b921b3c9ca71e001ce9f022d9ff1f14558
Paulo Fernando Puppe Junior <puppejunior@gmail.com>
2023-09-06 18:21:55 -0300

    Atualizar o WhatsAppButton.js

--- src/components/WhatsAppButton.js
+++ src/components/WhatsAppButton.js
@@ -7,7 +7,8 @@ const WhatsAppButton = ({ message }) => {
 
   return (
     <a href={whatsappLink} className="btn btn-success" target="_blank" rel="noopener noreferrer">
- Clique "AQUI" para confirmar o pedido se Tele: Endereço/Telefone
+ Clique "AQUI" para confirmar o pedido
+{'\n'} se Tele: Endereço/Telefone
     </a>
   );
 };
