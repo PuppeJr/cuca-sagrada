9dc983350154b0765ed5a0d4efe77ee6d9d7c3b0
PuppeJr <puppejunior@gmail.com>
2023-09-07 16:38:17 -0300

    Mensagem ao Cliente

--- src/components/WhatsAppButton.js
+++ src/components/WhatsAppButton.js
@@ -4,19 +4,16 @@ import PropTypes from 'prop-types';
 const WhatsAppButton = ({ message }) => {
   const whatsappLink = `https://wa.me/+5551989892020?text=${encodeURIComponent(message)}`;
 
-
   return (
-  <a href={whatsappLink} className="btn btn-success" target="_blank" rel="noopener noreferrer" style={{ whiteSpace: 'pre-line' }}>
-    {`Clique "AQUI" para confirmar a entrega {\n} Se Tele Entrega Endereço/Telefone: `}
-  </a>
-);
-
-
-
+    <a href={whatsappLink} className="btn btn-success" target="_blank" rel="noopener noreferrer">
+      Clique "Aqui" para confirmar o Pedido {'/n'}
+      Se Tele Entrega Endereço/Telefone
+    </a>
+  );
+};
 
 export default WhatsAppButton;
 
 WhatsAppButton.propTypes = {
   message: PropTypes.string.isRequired,
 };
-
