59955c6328c4a4ecbaff30ea662adc03dba39fe9
PuppeJr <puppejunior@gmail.com>
2023-09-07 17:11:22 -0300

    Mudando dizeres para o cliente

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,13 +15,13 @@ const Header = ({ cart, setShow }) => {
     <header className="bg-dark text-white fixed-top">
       <div className="container d-auto justify-content-between align-items-center py-2">
         <div className="company-name h1 mb-0">Cuca Sagrada</div>
-        <div className="company-name h0 mb-0">Tele-Entrega/Pagamento na entrega</div>
-        <div className="company-name h0 mb-0">ou Via PIX 51989892020</div>
+        <div className="company-name h0 mb-0">Tele-Entrega/Pagamento na entrega ou Via PIX:</div>
+        <div className="company-name h0 mb-0"><h3>51989892020</h3></div>
         <div className="company-name h0 mb-0">Rua Ari Marinho, 27/Higienópolis</div>
         <div className="cart text-end">
           <span className="cart-count me-3">{cart.length} itens</span>
           <span className="cart-total">R${total.toFixed(2)}</span>
-          <button className="btn btn-primary btn-sm" onClick={() => setShow(true)}>Veja seu pedido</button>
+          <button className="btn btn-primary btn-sm" onClick={() => setShow(true)}>Carrinho de Compras</button>
         </div>
       
       
